e07b1b6b92e3744c5427d70b4d36a9cfc17eccd0
yash <yashgouravkar@gmail.com>
2025-10-11 11:41:45 +0530

    meta

---
+++ src/lib/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Blair',
+  url: 'https://blair.ygkr.live',
+  ogImage: 'https://blair.ygkr.live/og.png',
+  description: 'AI based ghostwriting assistant.',
+  links: {
+    twitter: 'https://x.com/YashGouravKar1/',
+    github: 'https://github.com/99yash',
+    website: 'https://ygkr.live',
+  },
+} as const;
